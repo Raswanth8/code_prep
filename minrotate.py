591655dac343e3033d5adf2e830f2ae13665f486
Raswanth8 <raswanth8901@gmail.com>
2021-08-31 22:53:51 +0530

    Initial Commit🚀

--- minrotate.py
+++ minrotate.py
@@ -11,7 +11,8 @@ class Solution:
             if nums[l]<=nums[r]:
                 res = min(res,nums[l])
                 break
-            m = l+r // 2
+            m = (l+r) // 2
+            res = min(res,nums[m])
             if nums[m]>= nums[l]:
                 l = m + 1
             else :
