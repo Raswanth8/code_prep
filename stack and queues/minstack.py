b916ec33042760a2183cd91a080a93f7704f7e1b
Raswanth8 <raswanth8901@gmail.com>
2021-12-13 18:42:32 +0530

    Initial Commit🚀

---
+++ stack and queues/minstack.py
@@ -0,0 +1,27 @@
+# Min Stack
+"""
+Stack : -1 -> 0 -> -3
+minStack : -1 -> -1 -> -3
+Push the value in stack, get minimum element and push it into minStack.
+
+"""
+class MinStack:
+    
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val,self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
