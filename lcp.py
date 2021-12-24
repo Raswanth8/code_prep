ad7306932752884e3aef200d967c5c920bee0b28
Raswanth8 <raswanth8901@gmail.com>
2021-12-24 20:38:40 +0530

    Initial Commit🚀

---
+++ lcp.py
@@ -0,0 +1,18 @@
+# Longest Common Prefix
+"""
+The idea is to get common prefix from smallest string in the list
+
+"""
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        result = ""
+        
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return result
+            result += strs[0][i]
+        return result
+        
