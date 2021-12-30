8f80d62e54d99f09aa3ada9a7c1f257b2e0fa29d
Raswanth8 <raswanth8901@gmail.com>
2021-12-30 19:48:06 +0530

    Initial Commit🚀

---
+++ Arrays/mergesortarr.py
@@ -0,0 +1,32 @@
+# Merge Sorted Arrays
+"""
+Compare array elments in reverse order and swap (merge)
+O(N) Time Complexity
+
+"""
+from typing import List
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        # Get last element from nums1
+        last = m+n-1
+        
+        # merge in reverse order
+        while m > 0 and n > 0 :
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -=1
+            else:
+                nums1[last] = nums2[n-1]
+                n -=1
+            last -=1
+        
+        # append leftover nums2 to nums1 
+        while n >0 :
+            nums1[last] = nums2[n-1]
+            n -=1
+            last -=1
+          
